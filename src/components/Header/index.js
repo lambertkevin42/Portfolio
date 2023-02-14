e6ce4feffc13f2bf1eafc1acdd91eaae1b7a021e
lambertkevin42 <lambertkevin1994@gmail.com>
2023-02-14 16:47:10 +0100

    burger with content lorem done + style scss clean

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import '../Header/header.scss';
+import HeaderNav from '../HeaderNav';
 
 function Header() {
     return (
         <div className="header">
             <p className="header-text"><strong>KL</strong></p>
+            <HeaderNav/>
         </div>
     );
 }
--- src/components/HeaderNav/index.js
+++ src/components/HeaderNav/index.js
@@ -1,7 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { toggleBurger } from '../../actions/burgeractions';
+import '../HeaderNav/headernav.scss';
+
 function HeaderNav() {
+    const dispatch = useDispatch();
+    const { burgerIsOpen } = useSelector((state) => state);
+    const handleClick = () => {
+        dispatch(toggleBurger());
+    };
+
     return(
         <nav className="header-nav">
-
+            <ul className={`${burgerIsOpen ? 'header-nav-links' : 'header-nav-links--closed'}`}>
+                <NavLink
+                    className="header-nav-link"
+                    to=""
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    className="header-nav-link"
+                    to=""
+                >
+                    lorem
+                </NavLink>
+                <NavLink
+                    className="header-nav-link"
+                    to=""
+                >
+                    lorem
+                </NavLink>
+            </ul>
+            <button
+                aria-label="toggleBurger"
+                type="button"
+                className={`${burgerIsOpen ? 'header-nav-button' : 'header-nav-button--closed'}`}
+                onClick={handleClick}
+            />
         </nav>
     );
 }
